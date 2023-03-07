770d5cb7ccb870147ea73baa4a1067e8207bec1b
Hafiz Abdiel <73648392+dlzcods@users.noreply.github.com>
2023-03-07 17:42:27 +0700

    Update source code dengan total harga pesanan

--- latihan-daftar-menu.cpp
+++ latihan-daftar-menu.cpp
@@ -1,49 +1,55 @@
-#include <iostream>  // library untuk input output stream
-#include <vector>     // library untuk menggunakan vector pada program
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {         // fungsi utama dari program
+int main()
+{
+    int jmlh_menu, ttl_pesanan;
+    string nama_menu, nm_customer;
+    char ulang;
+    vector<string> daftar_menu;
+    vector<int> harga_menu;
 
-    int jmlh_menu, ttl_pesanan;   // deklarasi variabel bertipe integer
-    string nama_menu, nm_customer;  // deklarasi variabel bertipe string
-    char ulang;       // deklarasi variabel bertipe karakter
-    vector<string> daftar_menu;  // deklarasi variabel bertipe vector yang berisi string
+    do {
+        cout << "\n=================== Program Menampilkan Daftar Menu ===================\n";
+        cout << "\n Masukkan nama customer: ";
+        getline(cin, nm_customer);
 
-    do {  // perulangan do-while, selama kondisinya true, program akan dijalankan
+        cout << "\n Masukkan jumlah pesanan: ";
+        cin >> jmlh_menu;
+        cin.ignore();
 
-        cout << "\n=================== Program Menampilkan Daftar Menu ===================\n";  // menampilkan tulisan pada layar
-        cout << "\n Masukkan nama customer: ";  // menampilkan tulisan pada layar
-        getline(cin, nm_customer);  // mengambil input dari user dan dimasukkan ke dalam variabel nm_customer
+        for(int i = 1; i <= jmlh_menu; i++){
+            cout << "\n Masukkan nama pesanan ke-" << i << ": ";
+            getline(cin, nama_menu);
+            daftar_menu.push_back(nama_menu);
 
-        cout << "\n Masukkan jumlah pesanan: ";  // menampilkan tulisan pada layar
-        cin >> jmlh_menu;  // mengambil input dari user dan dimasukkan ke dalam variabel jmlh_menu
-        cin.ignore();  // menghapus karakter '\n' pada input sebelumnya
-
-        for(int i = 1; i <= jmlh_menu; i++){  // perulangan for, berjalan sebanyak jmlh_menu kali
-
-            cout << " Masukkan nama pesanan ke-" << i << ": ";  // menampilkan tulisan pada layar
-            getline(cin, nama_menu);  // mengambil input dari user dan dimasukkan ke dalam variabel nama_menu
-            daftar_menu.push_back(nama_menu);  // memasukkan nama_menu ke dalam vector daftar_menu
-
+            cout << " Masukkan harga pesanan ke-" << i << ": ";
+            int harga;
+            cin >> harga;
+            harga_menu.push_back(harga);
+            cin.ignore();
         }
 
-        cout << "\n=========================== Detail Pesanan ============================\n";  // menampilkan tulisan pada layar
-        cout << "\n Daftar Pesanan atas Nama: " << nm_customer << endl;  // menampilkan nama_customer
-        for(int i = 0; i < daftar_menu.size(); i++){  // perulangan for, berjalan sebanyak elemen pada vector daftar_menu
+        cout << "\n=========================== Detail Pesanan ============================\n";
+        cout << "\n Daftar Pesanan atas Nama: " << nm_customer << endl;
 
-            cout << " " << i+1 << ". " << daftar_menu[i] << endl;  // menampilkan nomor pesanan dan nama pesanan
-
+        int total_harga = 0;
+        for(int i = 0; i < daftar_menu.size(); i++){
+            cout << " " << i+1 << ". " << daftar_menu[i] << " - Rp " << harga_menu[i] << endl;
+            total_harga += harga_menu[i];
         }
 
-        cout << "\n Total Pesanan: " << jmlh_menu << endl;  // menampilkan jumlah pesanan
-        cout << "\n Mau Pesan Lagi? (y/n): ";  // menampilkan tulisan pada layar
-        cin >> ulang;  // mengambil input dari user dan dimasukkan ke dalam variabel ulang
-        cin.ignore();  // menghapus karakter '\n' pada input sebelumnya
+        cout << "\n Total Harga: Rp" << total_harga << endl;
+        cout << "\n Mau Pesan Lagi? (y/n): ";
+        cin >> ulang;
+        cin.ignore();
 
-        daftar_menu.clear();  // menghapus semua elemen pada vector daftar_menu
+        daftar_menu.clear();
+        harga_menu.clear();
 
-    } while (ulang == 'y'||ulang == 'Y');  // perulangan akan terus berjalan selama ulang bernilai 'y' atau 'Y'
+    } while (ulang == 'y'||ulang == 'Y');
 
-    return 0;  // program selesai dan mengembalikan nilai 0
+    return 0;
 }
